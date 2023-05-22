9077076652a09b463a42e965487ce9d49bd7f08d
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2023-05-22 17:39:07 +0200

    add more stuff

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,27 @@ const users: User[] = [
   { id: '3', name: 'User 3' },
 ];
 
-
-interface UserMap {
-  [id: string]: User;
+const userMapExample: Record<string, User> = {
+  '1': {
+    id: '1',
+    name: 'User 1',
+  },
+  '2': {
+    id: '2',
+    name: 'User 2',
+  },
+  '3': {
+    id: '3',
+    name: 'User 3',
+  },
 }
 
 
+const userMap: Record<string, User> = users.reduce<Record<string, User>>((acculator, user) => {
+  acculator[user.id] = user;
 
-const userMap: UserMap = users.reduce((acc, user) => {
-  acc[user.id] = user;
-  return acc;
-}, {} as UserMap);
+  return acculator;
+}, {} )
 
 userMap['1']; // { id: '1', name: 'User 1' }
 userMap['2']; // { id: '2', name: 'User 2' }
@@ -28,18 +38,20 @@ userMap['2']; // { id: '2', name: 'User 2' }
 const userMap2 = new Map<string, User>();
 
 users.forEach(user => {
-  userMap2.set(user.id, user);
-});
+  userMap2.set(user.id, user)
+})
 
 userMap2.get('1'); // { id: '1', name: 'User 1' }
 userMap2.get('2'); // { id: '2', name: 'User 2' }
 
-
 // Map spiegazione
 const map = new Map<'cat'| 'dog', number>();
 
+
 map.set('cat', 2);
 map.set('dog', 3);
+
+// {'cat': 2, 'dog': 3}
 
 map.entries(); // [ ['cat', 2], ['dog', 3] ]
 map.keys(); // ['cat', 'dog']
@@ -47,3 +59,5 @@ map.values(); // [2, 3]
 map.get('cat'); // 2
 map.get('dog'); // 3
 map.has('cat'); // true
+map.delete('cat'); // true
+
